58b79910c298f2ee032eff00d116a968449947ad
Frederik Sachser <f.sachser@gmx.at>
2019-11-28 11:36:06 +0100

    update grepall

--- R/grepall.R
+++ R/grepall.R
@@ -6,34 +6,45 @@
 #' @param print_values Logical. Choose wether to print values.
 #'
 #' @export
+#'
+#' @examples testdata <- data.frame(a = c(1, "?"), b = 1:2, C = c("?", "?"), d = c("?", "a"), stringsAsFactors = FALSE)
+#' index <- which(as.matrix(testdata) == "?", arr.ind = TRUE)
+#' testdata[index]
 grepall <- function(dataframe, string, ..., print_values = TRUE) {
-  charlist <- as.list(dataframe[sapply(dataframe, is.character)])
-  emptylist <- as.list(rep(NA, length(charlist)))
-  names(emptylist) <- names(charlist)
 
-  for (i in 1:length(charlist)) {
-    emptylist[[i]] <- grep(string, charlist[[i]], ...)
-  }
+  foo <- as.matrix(dataframe)
+  index1d <- which(grepl(pattern = string, x = foo, ...), arr.ind = TRUE)
+
+  fooma <- matrix(data = 0, nrow = dim(foo)[1], ncol = dim(foo)[2])
+  fooma[index1d] <- 1
+  index2d <- which(fooma == 1, arr.ind = TRUE)
 
   if (print_values == TRUE) {
-    for (i in names(charlist)) {
-      foo <- dataframe[emptylist[[i]], i]
-      if (nrow(foo) > 0)
-        print(foo)
-    }
+    print(dataframe[index2d])
   }
-  invisible(emptylist)
-  message("List with index returned invisible")
 
-  # possible better/faster alternative:
-  # testdata <- data.frame(a = c(1, "?"), b = 1:2, C = c("?", "?"), d = c("?", "a"))
-  #
-  #index <- which(as.matrix(testdata) == "?", arr.ind = TRUE)
-  #testdata[index]
+  message("List with index returned invisible")
+  return(invisible(index2d))
+
+#     dataframe <- dplyr::as_tibble(dataframe)
+#   charlist <- as.list(dataframe[sapply(dataframe, is.character)])
+#   emptylist <- as.list(rep(NA, length(charlist)))
+#   names(emptylist) <- names(charlist)
+#
+#   for (i in 1:length(charlist)) {
+#     emptylist[[i]] <- grep(string, charlist[[i]], ...)
+#   }
+#
+#   if (print_values == TRUE) {
+#     for (i in names(charlist)) {
+#       foo <- dataframe[emptylist[[i]], i]
+#       if (nrow(foo) > 0)
+#         print(foo)
+#     }
+#   }
+#   invisible(emptylist)
+#   message("List with index returned invisible")
+
 
 }
-
-
-
-
 
