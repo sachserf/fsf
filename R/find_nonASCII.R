addda4fc2d54850b454078aa6e83dda18bd41d8a
Frederik Sachser <f.sachser@gmx.at>
2020-01-06 18:36:14 +0100

    update find_nonASCII.R

--- R/find_nonASCII.R
+++ R/find_nonASCII.R
@@ -1,8 +1,11 @@
 #' find Non-ASCII characters within a file
 #'
+#'
 #' @param filepath Character. Specify path to the file that you want to check for Non-ASCII characters.
 #'
-#' @note use sapply to search for non ASCII characters in multiple files
+#' @note use sapply to search for non ASCII characters in multiple files.
+#'
+#' @seealso \code{\link[tools]{showNonASCIIfile}}
 #' @export
 find_nonASCII <- function(filepath) {
   inputfile <- readLines(filepath)
@@ -14,9 +17,11 @@ find_nonASCII <- function(filepath) {
   # prepare dataframe
   thelines <- which(sapply(bar, length) != 0)
   chars <- bar[thelines]
+  raw <- sapply(sapply(lapply(unlist(chars), charToRaw), paste, collapse = " "), paste0)
 
   # create dataframe
   df_out <-
-    data.frame(Line = rep(thelines, sapply(chars, length)), char = unlist(chars))
+    data.frame(Line = rep(thelines, sapply(chars, length)), char = unlist(chars), raw = raw)
   return(df_out)
 }
+
